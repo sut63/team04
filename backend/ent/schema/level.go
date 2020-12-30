39535764056c0d4e2a2d390804f49dc5ea6750d0
nutzann <b6011734@sut.ac.th>
2020-12-31 00:08:58 +0700

    เพิ่ม entity ชื่อ level ของระบบบันทึกพื้นที่เสี่ยง -close #92

---
+++ backend/ent/schema/level.go
@@ -0,0 +1,27 @@
+package schema
+
+import (
+ "github.com/facebookincubator/ent"
+ "github.com/facebookincubator/ent/schema/edge"
+ "github.com/facebookincubator/ent/schema/field"
+)
+
+// Level holds the schema definition for the Level entity.
+type Level struct {
+ ent.Schema
+}
+
+// Fields of the Level.
+func (Level) Fields() []ent.Field {
+ return []ent.Field{
+ field.String("LevelName"),
+ }
+}
+
+// Edges of the Level.
+func (Level) Edges() []ent.Edge {
+ return []ent.Edge{
+ edge.To("area", Area.Type),
+ }
+
+}
