7a231bece1269cdf75f2eea9b4d90425f970d892
methi2554 <43237073+methi2554@users.noreply.github.com>
2020-12-30 22:19:32 +0700

    แก้ไข entity drug.go ในระบบบันทึกข้อมูลยา - close #80

---
+++ backend/ent/schema/drug.go
@@ -0,0 +1,30 @@
+package schema
+
+import (
+	"github.com/facebookincubator/ent"
+	"github.com/facebookincubator/ent/schema/edge"
+	"github.com/facebookincubator/ent/schema/field"
+)
+
+// Drug holds the schema definition for the Drug entity.
+type Drug struct {
+	ent.Schema
+}
+
+// Fields of the Drug.
+func (Drug) Fields() []ent.Field {
+	return []ent.Field{
+		field.String("DrugName").Unique(),
+		field.String("Howto").Unique(),
+		field.String("Property").Unique(),
+	}
+}
+
+// Edges of the Drug.
+func (Drug) Edges() []ent.Edge {
+	return []ent.Edge{
+		edge.From("employee", Employee.Type).Ref("drug").Unique(),
+		edge.From("drugtype", DrugType.Type).Ref("drug").Unique(),
+		edge.From("disease", Disease.Type).Ref("drug").Unique(),
+	}
+}
