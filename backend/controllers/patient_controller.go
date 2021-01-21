5fe88ec83b72d8264090789735f8bc30d5a99c61
Sujitnapa21 <sujitnapa29@gmail.com>
2021-01-21 22:45:09 +0700

    ทำ Validators ของระบบ Patient -close #228

--- backend/controllers/patient_controller.go
+++ backend/controllers/patient_controller.go
@@ -128,15 +128,20 @@ func (ctl *PatientController) CreatePatient(c *gin.Context) {
 		SetEmployee(e).
 		Save(context.Background())
 
-	if err != nil {
-		c.JSON(400, gin.H{
-			"error": "saving failed",
+		if err != nil {
+			fmt.Println(err)
+			c.JSON(400, gin.H{
+				"status": false,
+				"error":  err,
+			})
+			return
+		}
+	
+		c.JSON(200, gin.H{
+			"status": true,
+			"data":   pa,
 		})
-		return
 	}
-
-	c.JSON(200, pa)
-}
 
 // ListPatient handles request to get a list of patient entities
 // @Summary List patient entities
